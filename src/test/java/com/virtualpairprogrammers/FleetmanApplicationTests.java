cbd24cc056e8502e3d9d050ddd2c42e0def5b4a2
dummy <dummy@commit.com>
2026-08-26 05:48:12 +0000

    Initial commit

---
+++ src/test/java/com/virtualpairprogrammers/FleetmanApplicationTests.java
@@ -0,0 +1,16 @@
+package com.virtualpairprogrammers;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@ExtendWith(SpringExtension.class)
+@WebAppConfiguration
+public class FleetmanApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
